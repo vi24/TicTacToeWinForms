7c370c7aceec2195ade157377f6c4e95f60555fd
Ravivarnen Sivasothilingam <31028156+vi24@users.noreply.github.com>
2019-05-17 11:06:58 +0200

    Forms Controls Loaded

---
+++ TicTacToe/TicTacToe.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Position1PicBox = new System.Windows.Forms.PictureBox();
+            this.Position2PicBox = new System.Windows.Forms.PictureBox();
+            this.Position3PicBox = new System.Windows.Forms.PictureBox();
+            this.Position6PicBox = new System.Windows.Forms.PictureBox();
+            this.Position5PicBox = new System.Windows.Forms.PictureBox();
+            this.Position4PicBox = new System.Windows.Forms.PictureBox();
+            this.Position9PicBox = new System.Windows.Forms.PictureBox();
+            this.Position8PicBox = new System.Windows.Forms.PictureBox();
+            this.Position7PicBox = new System.Windows.Forms.PictureBox();
+            this.JsonRadioButton = new System.Windows.Forms.RadioButton();
+            this.XmlRadioButton = new System.Windows.Forms.RadioButton();
+            this.ExportGroupBox = new System.Windows.Forms.GroupBox();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.ExportFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.TurnLabel = new System.Windows.Forms.Label();
+            this.PlayerVsBotButton = new System.Windows.Forms.Button();
+            this.PlayerVsPlayerButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Position1PicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position2PicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position3PicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position6PicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position5PicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position4PicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position9PicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position8PicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position7PicBox)).BeginInit();
+            this.ExportGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Position1PicBox
+            // 
+            this.Position1PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position1PicBox.Location = new System.Drawing.Point(0, 0);
+            this.Position1PicBox.Name = "Position1PicBox";
+            this.Position1PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position1PicBox.TabIndex = 0;
+            this.Position1PicBox.TabStop = false;
+            // 
+            // Position2PicBox
+            // 
+            this.Position2PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position2PicBox.Location = new System.Drawing.Point(171, 0);
+            this.Position2PicBox.Name = "Position2PicBox";
+            this.Position2PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position2PicBox.TabIndex = 1;
+            this.Position2PicBox.TabStop = false;
+            // 
+            // Position3PicBox
+            // 
+            this.Position3PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position3PicBox.Location = new System.Drawing.Point(342, 0);
+            this.Position3PicBox.Name = "Position3PicBox";
+            this.Position3PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position3PicBox.TabIndex = 2;
+            this.Position3PicBox.TabStop = false;
+            // 
+            // Position6PicBox
+            // 
+            this.Position6PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position6PicBox.Location = new System.Drawing.Point(342, 162);
+            this.Position6PicBox.Name = "Position6PicBox";
+            this.Position6PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position6PicBox.TabIndex = 3;
+            this.Position6PicBox.TabStop = false;
+            // 
+            // Position5PicBox
+            // 
+            this.Position5PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position5PicBox.Location = new System.Drawing.Point(171, 162);
+            this.Position5PicBox.Name = "Position5PicBox";
+            this.Position5PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position5PicBox.TabIndex = 4;
+            this.Position5PicBox.TabStop = false;
+            // 
+            // Position4PicBox
+            // 
+            this.Position4PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position4PicBox.Location = new System.Drawing.Point(0, 162);
+            this.Position4PicBox.Name = "Position4PicBox";
+            this.Position4PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position4PicBox.TabIndex = 5;
+            this.Position4PicBox.TabStop = false;
+            // 
+            // Position9PicBox
+            // 
+            this.Position9PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position9PicBox.Location = new System.Drawing.Point(342, 323);
+            this.Position9PicBox.Name = "Position9PicBox";
+            this.Position9PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position9PicBox.TabIndex = 6;
+            this.Position9PicBox.TabStop = false;
+            // 
+            // Position8PicBox
+            // 
+            this.Position8PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position8PicBox.Location = new System.Drawing.Point(171, 323);
+            this.Position8PicBox.Name = "Position8PicBox";
+            this.Position8PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position8PicBox.TabIndex = 7;
+            this.Position8PicBox.TabStop = false;
+            // 
+            // Position7PicBox
+            // 
+            this.Position7PicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Position7PicBox.Image = global::TicTacToe.Properties.Resources.X;
+            this.Position7PicBox.Location = new System.Drawing.Point(0, 323);
+            this.Position7PicBox.Name = "Position7PicBox";
+            this.Position7PicBox.Size = new System.Drawing.Size(165, 165);
+            this.Position7PicBox.TabIndex = 8;
+            this.Position7PicBox.TabStop = false;
+            // 
+            // JsonRadioButton
+            // 
+            this.JsonRadioButton.AutoSize = true;
+            this.JsonRadioButton.Location = new System.Drawing.Point(25, 40);
+            this.JsonRadioButton.Name = "JsonRadioButton";
+            this.JsonRadioButton.Size = new System.Drawing.Size(53, 17);
+            this.JsonRadioButton.TabIndex = 9;
+            this.JsonRadioButton.TabStop = true;
+            this.JsonRadioButton.Text = "JSON";
+            this.JsonRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // XmlRadioButton
+            // 
+            this.XmlRadioButton.AutoSize = true;
+            this.XmlRadioButton.Location = new System.Drawing.Point(25, 63);
+            this.XmlRadioButton.Name = "XmlRadioButton";
+            this.XmlRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.XmlRadioButton.TabIndex = 10;
+            this.XmlRadioButton.TabStop = true;
+            this.XmlRadioButton.Text = "XML";
+            this.XmlRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ExportGroupBox
+            // 
+            this.ExportGroupBox.Controls.Add(this.ExportButton);
+            this.ExportGroupBox.Controls.Add(this.JsonRadioButton);
+            this.ExportGroupBox.Controls.Add(this.XmlRadioButton);
+            this.ExportGroupBox.Location = new System.Drawing.Point(0, 494);
+            this.ExportGroupBox.Name = "ExportGroupBox";
+            this.ExportGroupBox.Size = new System.Drawing.Size(165, 137);
+            this.ExportGroupBox.TabIndex = 11;
+            this.ExportGroupBox.TabStop = false;
+            this.ExportGroupBox.Text = "Export File Format";
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Location = new System.Drawing.Point(25, 97);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(115, 23);
+            this.ExportButton.TabIndex = 11;
+            this.ExportButton.Text = "Export";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            // 
+            // TurnLabel
+            // 
+            this.TurnLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TurnLabel.Location = new System.Drawing.Point(279, 528);
+            this.TurnLabel.Name = "TurnLabel";
+            this.TurnLabel.Size = new System.Drawing.Size(100, 23);
+            this.TurnLabel.TabIndex = 12;
+            this.TurnLabel.Text = "Turn";
+            this.TurnLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PlayerVsBotButton
+            // 
+            this.PlayerVsBotButton.Location = new System.Drawing.Point(248, 567);
+            this.PlayerVsBotButton.Name = "PlayerVsBotButton";
+            this.PlayerVsBotButton.Size = new System.Drawing.Size(159, 23);
+            this.PlayerVsBotButton.TabIndex = 13;
+            this.PlayerVsBotButton.Text = "Start Player vs Bot";
+            this.PlayerVsBotButton.UseVisualStyleBackColor = true;
+            // 
+            // PlayerVsPlayerButton
+            // 
+            this.PlayerVsPlayerButton.Location = new System.Drawing.Point(248, 608);
+            this.PlayerVsPlayerButton.Name = "PlayerVsPlayerButton";
+            this.PlayerVsPlayerButton.Size = new System.Drawing.Size(159, 23);
+            this.PlayerVsPlayerButton.TabIndex = 14;
+            this.PlayerVsPlayerButton.Text = "Start Player vs Player";
+            this.PlayerVsPlayerButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 659);
+            this.Controls.Add(this.PlayerVsPlayerButton);
+            this.Controls.Add(this.PlayerVsBotButton);
+            this.Controls.Add(this.TurnLabel);
+            this.Controls.Add(this.Position8PicBox);
+            this.Controls.Add(this.ExportGroupBox);
+            this.Controls.Add(this.Position7PicBox);
+            this.Controls.Add(this.Position9PicBox);
+            this.Controls.Add(this.Position4PicBox);
+            this.Controls.Add(this.Position5PicBox);
+            this.Controls.Add(this.Position6PicBox);
+            this.Controls.Add(this.Position3PicBox);
+            this.Controls.Add(this.Position2PicBox);
+            this.Controls.Add(this.Position1PicBox);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Position1PicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position2PicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position3PicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position6PicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position5PicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position4PicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position9PicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position8PicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Position7PicBox)).EndInit();
+            this.ExportGroupBox.ResumeLayout(false);
+            this.ExportGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Position1PicBox;
+        private System.Windows.Forms.PictureBox Position2PicBox;
+        private System.Windows.Forms.PictureBox Position3PicBox;
+        private System.Windows.Forms.PictureBox Position6PicBox;
+        private System.Windows.Forms.PictureBox Position5PicBox;
+        private System.Windows.Forms.PictureBox Position4PicBox;
+        private System.Windows.Forms.PictureBox Position9PicBox;
+        private System.Windows.Forms.PictureBox Position8PicBox;
+        private System.Windows.Forms.PictureBox Position7PicBox;
+        private System.Windows.Forms.RadioButton JsonRadioButton;
+        private System.Windows.Forms.RadioButton XmlRadioButton;
+        private System.Windows.Forms.GroupBox ExportGroupBox;
+        private System.Windows.Forms.SaveFileDialog ExportFileDialog;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Label TurnLabel;
+        private System.Windows.Forms.Button PlayerVsBotButton;
+        private System.Windows.Forms.Button PlayerVsPlayerButton;
+    }
+}
+
--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace TicTacToe
 {
-    public partial class Form1 : Form
+    public partial class TicTacToe : Form
     {
-        public Form1()
+        public TicTacToe()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
